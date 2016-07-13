5a984393565dd601a930f1eb43f71847de8c723b
Hasibeee <hasibe_pnk@hotmail.com>
2016-07-13 13:53:03 +0300

    Item modeli oluşturuldu ve bağlatısı yapıldı

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160713085037) do
+ActiveRecord::Schema.define(version: 20160713103644) do
+
+  create_table "items", force: :cascade do |t|
+    t.text     "content"
+    t.boolean  "is_completed"
+    t.integer  "user_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
